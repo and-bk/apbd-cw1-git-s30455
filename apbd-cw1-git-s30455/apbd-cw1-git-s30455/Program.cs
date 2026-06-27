26adf5a0ba040d982946e773f3d99f4a09cd47c4
Andrii Bohomilchuk <abogomilchuk@gmail.com>
2026-06-27 11:32:26 +0200

    Implemented deviding

--- apbd-cw1-git-s30455/apbd-cw1-git-s30455/Program.cs
+++ apbd-cw1-git-s30455/apbd-cw1-git-s30455/Program.cs
@@ -3,10 +3,25 @@ string name = Console.ReadLine();
 Console.WriteLine("Enter your age:");
 int age = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter first number:");
-int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter second number:");
-int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Choose operation(x/:): ");
+string operation = Console.ReadLine();
 
-Console.WriteLine($"{name}, {age}");
-Console.WriteLine($"{num1} x {num2} =  {num1 * num2}");
+if (operation == "x")
+{
+    Console.WriteLine("Enter first number:");
+    int num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter second number:");
+    int num2 = int.Parse(Console.ReadLine());
+    
+    Console.WriteLine($"{name}, {age}");
+    Console.WriteLine($"{num1} x {num2} =  {num1 * num2}");
+} else if (operation == ":")
+{
+    Console.WriteLine("Enter first number:");
+    int num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter second number:");
+    int num2 = int.Parse(Console.ReadLine());
+    
+    Console.WriteLine($"{name}, {age}");
+    Console.WriteLine($"{num1} : {num2} =  {num1 / num2}");
+}
