88c9a7c4feeda08cba444b223489dad168c20f6b
Andrii Bohomilchuk <abogomilchuk@gmail.com>
2026-06-27 11:29:15 +0200

    Implemented mulipliyng

--- apbd-cw1-git-s30455/apbd-cw1-git-s30455/Program.cs
+++ apbd-cw1-git-s30455/apbd-cw1-git-s30455/Program.cs
@@ -2,4 +2,11 @@
 string name = Console.ReadLine();
 Console.WriteLine("Enter your age:");
 int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter first number:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+int num2 = int.Parse(Console.ReadLine());
+
 Console.WriteLine($"{name}, {age}");
+Console.WriteLine($"{num1} x {num2} =  {num1 * num2}");
